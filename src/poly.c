540a6a9bfd2a897b46562153cb691a6e2be09809
Tudny <aleksanderwt@gmail.com>
2021-04-11 15:18:55 +0200

    Added TODO and file information for Doxygen.

---
+++ src/poly.c
@@ -0,0 +1,20 @@
+/** @file
+  Implementacja biblioteki podstawowych operacji na wielomianach rzadkich wielu zmiennych.
+
+  @author Aleksander Tudruj <at429620@students.mimuw.edu.pl>
+  @date 2021
+ */
+
+// TODO
+// PolyDestroy
+// PolyClone
+// PolyAdd
+// PolyAddMonos
+// PolyMul
+// PolyNeg
+// PolySub
+// PolyDegBy
+// PolyDeg
+// PolyIsEq
+// PolyAt
+
