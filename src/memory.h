45e72d3d149c073c633c789e575430eeac664dba
Tudny <aleksanderwt@gmail.com>
2021-04-24 21:01:40 +0200

    Added memory menagment module

---
+++ src/memory.h
@@ -0,0 +1,30 @@
+#ifndef __MEMORY_H__
+#define __MEMORY_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+// Sprawdzenie czy wskaźnik jest NULL'em. W przypadku pozytywnym
+// program zamykany jest z kodem błędu 1.
+//  ptr - sprawdzany wskaźnik
+void requireNotNull(void *ptr);
+
+// Bezpieczna alternatywa malloc'a. W przypadku zwrócenia przez malloc'a
+// NULL'a program zamykany jest z kodem błędu 1.
+//  memoryBlockSize - malloc'owany rozmiar
+void *safeMalloc(size_t memoryBlockSize);
+
+// Bezpieczna alternatywa calloc'a. W przypadku zwrócenia przez calloc'a
+// NULL'a program zamykany jest z kodem błędu 1.
+//  numberOfElements - liczba elementów do alokacji
+//  sizeOfElement    - rozmiar elementu
+void *safeCalloc(size_t numberOfElements, size_t sizeOfElement);
+
+// Bezpieczna alternatywa realloc'a. W przypadku Zwrócenia przez realloc'a
+// NULL'a program zamykany jest z kodem błędu 1.
+//  ptr             - wskaźnik na poprzednią pamięć
+//  memoryBlockSize - nowy rozmiar
+void *safeRealloc(void *ptr, size_t memoryBlockSize);
+
+#endif /* MEMORY_H */
+
