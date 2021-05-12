851b71417eafd73e66829221f20a1f29e87e18b6
Aleksander Tudruj <at429630@students.mimuw.edu.pl>
2021-05-13 00:26:23 +0200

    Added Stack module with comments

--- src/stack.c
+++ src/stack.c
@@ -1,5 +1,88 @@
-//
-// Created by tudny on 12.05.2021.
-//
-
+#include <stdio.h>
 #include "stack.h"
+
+static SElem *createSElem(Poly *p) {
+    SElem *sElem = safeMalloc(sizeof(SElem));
+    sElem->p = *p;
+
+    return sElem;
+}
+
+static void destorySElem(SElem *sElem) {
+    if (sElem == NULL)
+        return;
+
+    destorySElem(sElem->next);
+
+    PolyDestroy(&sElem->p);
+    safeFree((void **) &sElem);
+}
+
+static void printSElem(SElem *sElem) {
+    if (sElem == NULL) {
+        printf("TAIL{null}");
+        return;
+    }
+
+    printf("Node{");
+    PrintPolyNormalized(&sElem->p);
+    printf("}-> ");
+
+    printSElem(sElem->next);
+}
+
+Stack *createEmptyStack() {
+    Stack *stack = safeMalloc(sizeof(Stack));
+    stack->head = NULL;
+    stack->size = 0;
+
+    return stack;
+}
+
+void destoryStack(Stack *stack) {
+    destorySElem(stack->head);
+    safeFree((void **) &stack);
+}
+
+void pushStack(Stack *stack, Poly p) {
+    SElem *sElem = createSElem(&p);
+
+    sElem->next = stack->head;
+    stack->head = sElem;
+    ++stack->size;
+}
+
+bool isEmptyStack(Stack *stack) {
+    return sizeStack(stack) == 0;
+}
+
+size_t sizeStack(Stack *stack) {
+    return stack->size;
+}
+
+void popStack(Stack *stack) {
+    assert(!isEmptyStack(stack));
+
+    SElem *sElem = stack->head;
+    stack->head = stack->head->next;
+    --stack->size;
+
+    destorySElem(sElem);
+}
+
+Poly topStack(Stack *stack) {
+    assert(!isEmptyStack(stack));
+
+    return stack->head->p;
+}
+
+Poly takeStack(Stack *stack) {
+    Poly p = topStack(stack);
+    popStack(stack);
+    return p;
+}
+
+void printStack(Stack *stack) {
+    printSElem(stack->head);
+    printf("\n");
+}
