9bcdb82fe264fc07e323e7aa4663dc73804b44fa
Alek Tudruj <tudny@pop-os.localdomain>
2021-05-15 22:14:10 +0200

    added comments for calc.c

--- src/calc.c
+++ src/calc.c
@@ -1,9 +1,24 @@
+/** @file
+ * @brief Klakulator operujący na wielomianach.
+ * Plik główny kalkulatora działającego na wielomianach rzadkich wielu zmiennch
+ * stosującego odwrotną notację polską.
+ *
+ * @author Aleksander Tudruj <at429630@students.mimuw.edu.pl>
+ * */
+
 #include <stdlib.h>
 #include "memory.h"
 #include "reader.h"
 #include "input_handler.h"
 #include "stack.h"
 
+/**
+ * Główna funkcja uruchomieniowa.
+ * Zawiera główną część działania programu. Tworzy strukturę stosu, przetwarza
+ * wiersze oraz czyście pamięć przed zamknęciem pliku.
+ * @return kod wykonania programu; 0, gdy program zakończony się poprawnie
+ * 1, gdy program nie zakończy się poprawnie.
+ * */
 int main(void) {
 
     Stack *stack = createEmptyStack();
